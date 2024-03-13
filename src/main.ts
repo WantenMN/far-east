acfbffc76455018bc22b7428e283fc438de26c4f
Wanten <WantenMN@gmail.com>
2024-03-13 18:44:23 +0800

    chore: add event listener to main

--- src/main.ts
+++ src/main.ts
@@ -10,147 +10,147 @@ import * as Fonts from "./fonts";
 import * as Sounds from "./sound";
 import * as Game from "./game";
 
-const {
-  startSketch,
-  p,
-  canvas,
-  createPixels,
-  replaceCanvasPixels,
-  // pauseOrResume,
-  MoveKeys,
-  Mouse,
-} = window.p5Extension;
+addEventListener("p5ExtensionLoaded", () => {
+  const {
+    startSketch,
+    p,
+    canvas,
+    createPixels,
+    replaceCanvasPixels,
+    MoveKeys,
+    Mouse,
+  } = window.p5Extension;
 
-// ---- variables | functions ----
+  // ---- variables | functions ----
+  let drawBackground: () => void;
+  let volumeSlider: p5.Element;
+  let gameIsStarted = false;
 
-let drawBackground: () => void;
-let volumeSlider: p5.Element;
-let gameIsStarted = false;
+  // ---- reset & initialize ----
+  const reset = (): void => {
+    Game.reset(gameIsStarted);
+  };
 
-// ---- reset & initialize ----
-const reset = (): void => {
-  Game.reset(gameIsStarted);
-};
+  const updateVolume = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Sounds.setVolume((volumeSlider.value() as any as number) * 0.01);
+  };
 
-const updateVolume = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  Sounds.setVolume((volumeSlider.value() as any as number) * 0.01);
-};
+  const initializeVolumeSlider = () => {
+    volumeSlider = p.createSlider(0, 100, 15, 5);
+    volumeSlider.position(
+      canvas.scaleFactor * 50,
+      canvas.scaleFactor * (canvas.logicalSize.height - 40)
+    );
+    volumeSlider.style("width", `${canvas.scaleFactor * 200}px`);
+    volumeSlider.style("height", `${canvas.scaleFactor * 25}px`);
+    updateVolume();
+  };
 
-const initializeVolumeSlider = () => {
-  volumeSlider = p.createSlider(0, 100, 15, 5);
-  volumeSlider.position(
-    canvas.scaleFactor * 50,
-    canvas.scaleFactor * (canvas.logicalSize.height - 40)
-  );
-  volumeSlider.style("width", `${canvas.scaleFactor * 200}px`);
-  volumeSlider.style("height", `${canvas.scaleFactor * 25}px`);
-  updateVolume();
-};
-
-const initialize = (): void => {
-  const backgroundPixels = createPixels(() => {
-    canvas.drawScaled(() => {
-      const { width, height } = LOGICAL_CANVAS_SIZE;
-      p.background(248);
-      p.stroke(128, 112, 96, 4);
-      p.strokeCap(p.SQUARE);
-      p.strokeWeight(20);
-      for (let i = 0; i < 2000; i += 1) {
-        const x = p.random(0, width);
-        const y = p.random(-50, height - 50);
-        p.line(x, y, x, y + p.random(50, 200));
-      }
+  const initialize = (): void => {
+    const backgroundPixels = createPixels(() => {
+      canvas.drawScaled(() => {
+        const { width, height } = LOGICAL_CANVAS_SIZE;
+        p.background(248);
+        p.stroke(128, 112, 96, 4);
+        p.strokeCap(p.SQUARE);
+        p.strokeWeight(20);
+        for (let i = 0; i < 2000; i += 1) {
+          const x = p.random(0, width);
+          const y = p.random(-50, height - 50);
+          p.line(x, y, x, y + p.random(50, 200));
+        }
+      });
     });
+    drawBackground = () => replaceCanvasPixels(backgroundPixels);
+
+    p.imageMode(p.CENTER);
+    p.textFont(Fonts.en);
+
+    initializeVolumeSlider();
+
+    reset();
+  };
+
+  // ---- draw ----
+
+  const drawInstruction = () => {
+    p.push();
+
+    p.textFont(Fonts.en, 24);
+    p.textAlign(p.LEFT);
+
+    p.text("ARROW / WASD :", 160, 500);
+    p.text("MOVE", 460, 500);
+    p.text("Z / J / SPACE / ENTER :", 160, 540);
+    p.text("SHOOT", 460, 540);
+
+    p.text("PRESS SPACE KEY TO START", 160, 600);
+
+    p.pop();
+  };
+
+  const drawSketch = (): void => {
+    Game.run();
+
+    if (!gameIsStarted) drawInstruction();
+
+    p.textFont(Fonts.en, 16);
+    p.textAlign(p.LEFT);
+    p.text("VOL", 10, canvas.logicalSize.height - 23);
+  };
+
+  const draw = (): void => {
+    drawBackground();
+    canvas.drawScaled(drawSketch);
+    updateVolume();
+  };
+
+  // ---- UI ----
+
+  const keyTyped = (): void => {
+    switch (p.key) {
+      // case "p":
+      //   pauseOrResume();
+      //   break;
+      case "g":
+        p.save("image.png");
+        break;
+    }
+
+    if (!gameIsStarted && p.keyCode === 32) {
+      Game.reset(true);
+      gameIsStarted = true;
+    }
+
+    if (Game.state === Game.State.RESULT) Game.reset(true);
+  };
+
+  // ---- start sketch ----
+
+  const setP5Methods = (p: p5): void => {
+    p.draw = draw;
+    p.mouseMoved = () => {
+      Mouse.updatePosition();
+      Mouse.onMoved();
+    };
+    p.mousePressed = Mouse.onPressed;
+    p.keyTyped = keyTyped;
+    p.keyPressed = () => {
+      MoveKeys.onPressed();
+      if (MoveKeys.up || MoveKeys.left || MoveKeys.down || MoveKeys.right)
+        return false;
+    };
+    p.keyReleased = () => {
+      MoveKeys.onReleased();
+    };
+  };
+
+  startSketch({
+    htmlElement: HTML_ELEMENT,
+    logicalCanvasSize: LOGICAL_CANVAS_SIZE,
+    initialize,
+    setP5Methods,
+    fittingOption: ENABLE_CANVAS_SCALING ? undefined : null,
   });
-  drawBackground = () => replaceCanvasPixels(backgroundPixels);
-
-  p.imageMode(p.CENTER);
-  p.textFont(Fonts.en);
-
-  initializeVolumeSlider();
-
-  reset();
-};
-
-// ---- draw ----
-
-const drawInstruction = () => {
-  p.push();
-
-  p.textFont(Fonts.en, 24);
-  p.textAlign(p.LEFT);
-
-  p.text("ARROW / WASD :", 160, 500);
-  p.text("MOVE", 460, 500);
-  p.text("Z / J / SPACE / ENTER :", 160, 540);
-  p.text("SHOOT", 460, 540);
-
-  p.text("PRESS SPACE KEY TO START", 160, 600);
-
-  p.pop();
-};
-
-const drawSketch = (): void => {
-  Game.run();
-
-  if (!gameIsStarted) drawInstruction();
-
-  p.textFont(Fonts.en, 16);
-  p.textAlign(p.LEFT);
-  p.text("VOL", 10, canvas.logicalSize.height - 23);
-};
-
-const draw = (): void => {
-  drawBackground();
-  canvas.drawScaled(drawSketch);
-  updateVolume();
-};
-
-// ---- UI ----
-
-const keyTyped = (): void => {
-  switch (p.key) {
-    // case "p":
-    //   pauseOrResume();
-    //   break;
-    case "g":
-      p.save("image.png");
-      break;
-  }
-
-  if (!gameIsStarted && p.keyCode === 32) {
-    Game.reset(true);
-    gameIsStarted = true;
-  }
-
-  if (Game.state === Game.State.RESULT) Game.reset(true);
-};
-
-// ---- start sketch ----
-
-const setP5Methods = (p: p5): void => {
-  p.draw = draw;
-  p.mouseMoved = () => {
-    Mouse.updatePosition();
-    Mouse.onMoved();
-  };
-  p.mousePressed = Mouse.onPressed;
-  p.keyTyped = keyTyped;
-  p.keyPressed = () => {
-    MoveKeys.onPressed();
-    if (MoveKeys.up || MoveKeys.left || MoveKeys.down || MoveKeys.right)
-      return false;
-  };
-  p.keyReleased = () => {
-    MoveKeys.onReleased();
-  };
-};
-
-startSketch({
-  htmlElement: HTML_ELEMENT,
-  logicalCanvasSize: LOGICAL_CANVAS_SIZE,
-  initialize,
-  setP5Methods,
-  fittingOption: ENABLE_CANVAS_SCALING ? undefined : null,
 });
